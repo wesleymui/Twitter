dbb5b08c1b3cde3870ea9be5abfdb74880aca3fa
wesleymui <68392398+wesleymui@users.noreply.github.com>
2021-02-28 14:38:14 -0500

    test

---
+++ Twitter/LoginViewController.swift
@@ -0,0 +1,33 @@
+//
+//  LoginViewController.swift
+//  Twitter
+//
+//  Created by Wesley Mui on 2/28/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func onLoginButton(_ sender: Any) {
+        print("Hello World")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
